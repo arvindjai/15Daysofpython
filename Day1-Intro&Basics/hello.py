1984cdc06bd7e999596b489867071ab963fd850b
Arvind Jaiswal <26666567+arvindjai@users.noreply.github.com>
2023-10-29 14:04:45 +0530

    Create hello.py

---
+++ Day1-Intro&Basics/hello.py
@@ -0,0 +1,15 @@
+# Q.1 Write a Python program to print "Hello, World!
+
+print("Hello World")
+
+# Q.2: Calculate the sum of two numbers entered by the user.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter Second number: "))
+add = a + b
+print("Sum of two number is {}".format(add))
+
+# Q.3: Convert temperature from Celsius to Fahrenheit.
+celsius = float(input("Enter Temperature in Celsius: "))
+Ferh = celsius * (9/5) + 32
+print(f"{celsius} °C = {Ferh} °F ")
